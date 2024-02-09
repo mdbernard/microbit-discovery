7b5ca72a26996bbc7a2d7c7ce99f0d08a29eef95
Mike Bernard <michael.bernard@uconn.edu>
2024-02-08 22:42:07 -0800

    solve chapter 5: snake roulette

--- microbit/src/05-led-roulette/src/main.rs
+++ microbit/src/05-led-roulette/src/main.rs
@@ -3,15 +3,49 @@
 #![no_std]
 
 use cortex_m_rt::entry;
-use panic_halt as _;
-use microbit as _;
+use rtt_target::{rtt_init_print, rprintln};
+use panic_rtt_target as _;
+use microbit::{
+    board::Board,
+    display::blocking::Display,
+    hal::{prelude::*, Timer},
+};
 
 #[entry]
 fn main() -> ! {
-    let _y;
-    let x = 42;
-    _y = x;
+    rtt_init_print!();
+ 
+    let board = Board::take().unwrap();
+    let mut timer = Timer::new(board.TIMER0);
+    let mut display = Display::new(board.display_pins);
 
-    // infinite loop; just so we don't leave this stack frame
-    loop {}
+    let mut image = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+    ];
+
+    let mut row = 0;
+    let mut col = 0;
+
+    loop {
+        image[row][col] = 0;
+
+        if col < 4 && row == 0 {
+            col += 1;
+        } else if 0 < col  && row == 4 {
+            col -= 1;
+        } else if col == 4 && row < 4 {
+            row += 1;
+        } else if col == 0 && row > 0 {
+            row -= 1;
+        }
+
+        image[row][col] = 1;
+
+        display.show(&mut timer, image, 1000);
+    }
 }
+
